33f3039e2fffaeabd8910130178cf1a0d60b7a32
d.lyanguzov <d.lyanguzov@geoscan.ru>
2025-04-19 22:01:48 +0300

    update events api

--- src/data/Event.ts
+++ src/data/Event.ts
@@ -1,10 +1,30 @@
-export interface Event {
+export interface InstantEvent {
     id: Number,
     name: String,
-    records: Array<EventRecord>
+    records: Array<InstantRecord>
 }
 
-export interface EventRecord {
+export interface InstantRecord {
     timestamp: Number,
-    id: Number
+    data: Number
+}
+
+export interface RangedEvent {
+    id: Number,
+    name: String,
+    records: Array<RangedRecord>
+}
+
+export interface RangedRecord {
+    timestamp: Number,
+    data: Number,
+    end_event_id: Number,
+    end_event_name: String,
+    end_timestamp: Number,
+    end_data: Number
+}
+
+export interface EventsDTO {
+    instant: Array<InstantEvent>,
+    ranged: Array<RangedEvent>
 }
