9552b33ded5275a5e8ab55cd15c4540eb6f0f032
d.lyanguzov <d.lyanguzov@geoscan.ru>
2025-05-03 13:32:25 +0300

    Refactor InstantEventChart, make it a histogram

--- src/components/InstantEventChart.tsx
+++ src/components/InstantEventChart.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
+import {
+    XAxis,
+    YAxis,
+    Tooltip,
+    CartesianGrid,
+    ResponsiveContainer,
+    BarChart, Bar
+} from 'recharts';
 import {InstantEvent} from "../data/event";
 
 const InstantEventChart: React.FC<{ event: InstantEvent }> = ({ event }) => {
     return (
-        <LineChart width={600} height={300} data={event.records}>
-            <XAxis dataKey="timestamp" />
-            <YAxis />
-            <Tooltip />
-            <CartesianGrid strokeDasharray="3 3" />
-            <Line type="monotone" dataKey="data" stroke="#8884d8" />
-        </LineChart>
+            <ResponsiveContainer width={600} height={400}>
+                <BarChart data={event.records}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="timestamp" />
+                    <YAxis />
+                    <Tooltip />
+                    <Bar dataKey="data" fill="#8884d8" />
+                </BarChart>
+            </ResponsiveContainer>
+
     );
 };
 
